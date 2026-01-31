11f87f41e430835b941e64179c625e76cdceabbf
hooffaber <hooffaber@gmail.com>
2026-01-31 23:49:31 +0300

    Disable JWT token rotation

--- backend/src/settings/auth.py
+++ backend/src/settings/auth.py
@@ -17,8 +17,8 @@ AUTH_PASSWORD_VALIDATORS = [
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=env.int('JWT_ACCESS_LIFETIME_HOURS', default=24)),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=env.int('JWT_REFRESH_LIFETIME_DAYS', default=7)),
-    'ROTATE_REFRESH_TOKENS': True,
-    'BLACKLIST_AFTER_ROTATION': True,
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
     'AUTH_HEADER_TYPES': ('Bearer',),
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
 }
